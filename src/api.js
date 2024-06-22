4ac3b852c3ba58edce03a55d9a1db04345ed65be
truong <nguyenquangtruong26012003@gmail.com>
2024-06-22 18:30:40 +0700

    add prj

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "61876a714bmsh82702d704311ee4p13759ajsn0230bf8cf7b6",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "1a3e2e5d86b539c84c338ca22638c8af";
